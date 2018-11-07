d38aa0f23ee1a5d0be9d5ec6f2d5c888b219eaeb
Takakuwa <729534@cognizant.com>
2018-11-07 08:09:15 -0200

    init

---
+++ src/semana5/SeleniumWebDrive.java
@@ -0,0 +1,19 @@
+package semana5;
+
+/**
+*   Escreva 10 metodos do Selenium que façam ações na pagina web da Cognizant
+ *  Entre esses métodos tenha pelo menos um método para
+ *  -Procurar um elemento por id
+ *  -Clicar em um elemento da pagina html
+ *  -Usar um elemento WebDriver
+ *  -Escrever em um campo de texto
+ *
+ *  Com esses metodos que você criou, tente abrir a sua timesheet
+ */
+pubic class SeleniumWebDrive{
+
+    public static void main(String[] args) {
+
+
+    }
+}
