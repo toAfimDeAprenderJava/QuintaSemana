75d7798350cf058f5c4919165e91913c57881b9f
Cesar Monti <cesarmonti17@gmail.com>
2018-11-09 15:05:03 -0200

    Exercicio finalizado

--- src/test/java/test/ExercicioSemana5.java
+++ src/test/java/test/ExercicioSemana5.java
@@ -22,14 +22,19 @@ public class ExercicioSemana5 {
         //Abrir a página do LinkedIn
         navegador.get("https://www.linkedin.com");
         
+        //Preencher o campo do primeiro nome
         navegador.findElement(By.id("reg-firstname")).sendKeys("César");
         
+        //Preencher o campo do sobrenome
         navegador.findElement(By.id("reg-lastname")).sendKeys("Augusto");
         
+        //Preencher o campo de email
         navegador.findElement(By.id("reg-email")).sendKeys("teste.teste@teste.com");
         
+        //Preencher o campo de senha
         navegador.findElement(By.id("reg-password")).sendKeys("123456abc");
         
+        //Clicar no botão de cadastrar
         navegador.findElement(By.id("registration-submit")).click();
 
 }
