f406da759fb56ac915b4494f859751e2170136c3
Cesar Monti <cesarmonti17@gmail.com>
2018-11-09 15:02:18 -0200

    Exercicio completo

--- src/test/java/test/ExercicioSemana5.java
+++ src/test/java/test/ExercicioSemana5.java
@@ -1,13 +1,14 @@
 package test;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ExercicioSemana5 {
 	@Test
 	
-	public void fazerLogin(){
+	public void testeFazerCadastroLinkedIn(){
         
 		//Localizaçao do executável do navegador
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\725999\\drivers\\chromedriver.exe");
@@ -18,8 +19,18 @@ public class ExercicioSemana5 {
 		//Maximizando o navegador
         navegador.manage().window().maximize();
 
-        //Abrir a página do Google
-        navegador.get("https://www.google.com");
+        //Abrir a página do LinkedIn
+        navegador.get("https://www.linkedin.com");
+        
+        navegador.findElement(By.id("reg-firstname")).sendKeys("César");
+        
+        navegador.findElement(By.id("reg-lastname")).sendKeys("Augusto");
+        
+        navegador.findElement(By.id("reg-email")).sendKeys("teste.teste@teste.com");
+        
+        navegador.findElement(By.id("reg-password")).sendKeys("123456abc");
+        
+        navegador.findElement(By.id("registration-submit")).click();
 
 }
 }
